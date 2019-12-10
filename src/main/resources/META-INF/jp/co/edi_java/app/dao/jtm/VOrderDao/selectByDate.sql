e8588bb5e0656ec24f4c7106aa85472ff46e864d
shinji.yamaguchi <shinji-yamaguchi@tamahome.jp>
2019-12-10 14:14:36 +0900

    fix search

--- src/main/resources/META-INF/jp/co/edi_java/app/dao/jtm/VOrderDao/selectByDate.sql
+++ src/main/resources/META-INF/jp/co/edi_java/app/dao/jtm/VOrderDao/selectByDate.sql
@@ -1,16 +1,6 @@
-SELECT
-VO2.*
-FROM (
-	SELECT
-		ROW_NUMBER() OVER(ORDER BY VO.KOUJI_CODE, VO.DENPYOU_NO) rownumber,
-		VO.*
-	FROM
-		V_ORDER VO
-	WHERE
-	/*%if prevDate != null*/
-	VO.SAISYUU_KOUSIN_DATE >= /*prevDate*/'2019-08-01'
-	/*%end*/
-) VO2
+SELECT * FROM V_ORDER
 WHERE
-VO2.rownumber BETWEEN /*from*/1 AND /*to*/2
+/*%if prevDate != null*/
+SAISYUU_KOUSIN_DATE >= /*prevDate*/'2019-08-01'
+/*%end*/
 ;
